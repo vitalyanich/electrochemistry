3afafa599aa7d17f6856924b6503e4f6426295c7
vitalyanich <40563379+vitalyanich@users.noreply.github.com>
2019-05-29 02:06:03 +0300

    compute distributions released

--- GerisherMarkus.py
+++ GerisherMarkus.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.integrate as integrate
 from scipy.optimize import minimize
+import os
+import matplotlib.pyplot as plt
 
 
 class GM:
@@ -26,31 +28,35 @@ class GM:
         self.T = None
         self.l = None
 
-        self.E_DOS = None
+        self.E = None
+        self.DOS = None
+
+        self.sigma_eq = None
+        elf.dE_Q_eq = None
         self.y_fermi = None
         self.y_redox = None
 
         if DOS is None:
             try:
-                self.DOS = np.load('Saved_data/DOS.npy').item()
+                self.DOS = np.load('Saved_data/DOS.npy')
             except OSError:
                 print('File DOS.npy does not exist')
 
         if E is None:
             try:
-                self.E = np.load('Saved_data/E.npy').item()
+                self.E = np.load('Saved_data/E.npy')
             except OSError:
                 print('File E_DOS.npy does not exist')
 
         if efermi is None:
             try:
-                self.efermi = np.load('Saved_data/efermi.npy').item()
+                self.efermi = np.load('Saved_data/efermi.npy')
             except OSError:
                 print('File efermi.npy does not exist')
 
         if vacuum_lvl is None:
             try:
-                self.vacuum_lvl = np.load('Saved_data/vacuum_lvl.npy').item()
+                self.vacuum_lvl = np.load('Saved_data/vacuum_lvl.npy')
             except OSError:
                 print('File vacuum_lvl.npy does not exist')
 
@@ -74,17 +80,55 @@ class GM:
 
         self.sheet_area = sheet_area
 
+    def save(self, variable='all', dir='Saved_data', postfix=''):
+        """
+        This function saves variables in .npy files
+        :param variable: desired variable
+        :param dir: directory name to save file
+        :param postfix: postfix for saved files
+        :return: nothing
+        """
+        if variable == 'all':
+            for var in ['y_fermi', 'y_redox']:
+                if self.var is not None:
+                    self.save(var, postfix=postfix)
+        else:
+            if not os.path.exists(dir):
+                print('Directory ', dir, ' does not exist. Creating directory')
+                os.mkdir(dir)
+            np.save(dir + '/' + variable + postfix + '.npy', getattr(self, variable))
+            print('Variable ', variable, ' saved to directory ', dir)
+
+    def load(self, variable='all', dir='Saved_data', postfix=''):
+        """
+        This function loads variables from .npy files to class variables
+        :param variable: desired variable
+        :param dir: directory from which load files
+        :param postfix: postfix for loading files
+        :return: nothing
+        """
+        if variable == 'all':
+            for var in ['y_fermi', 'y_redox']:
+                self.load(var, postfix=postfix)
+        else:
+            setattr(self, variable, np.load(dir + '/' + str(variable) + postfix + '.npy'))
+            print('Variable ', variable, ' loaded')
+
+    def check_param(self, variable):
+        """
+        This function checks whether desires variable is not None and if necessary load it from file or process VASP data
+        :param variable: desired variable
+        :return: nothing
+        """
+        if getattr(self, variable) is None:
+            raise ValueError(f'{variable} is not defined')
+
     @staticmethod
     def nearest_array_indices(array, value):
         i = 0
         while value < array[i]:
             i += 1
         return i - 1, i
-
-    @staticmethod
-    def compute_sigma_EDL(C_EDL, dE_EDL):
-
-        return C_EDL * dE_EDL
 
     @staticmethod
     def fermi_func(E, T):
@@ -103,7 +147,11 @@ class GM:
         W_0 = (1 / np.sqrt(4 * k * T * l))
         return W_0 * np.exp(- (E + l) ** 2 / (4 * k * T * l))
 
-    def compute_sigma_quantum(self, dE_Q_arr, T, E_DOS, DOS, efermi):
+    def compute_sigma_EDL(self, dE_EDL):
+
+        return self.C_EDL * dE_EDL
+
+    def compute_sigma_quantum(self, dE_Q_arr):
         """Compute surface charge density induced by depletion or excess of electrons
 
         Parameters:
@@ -111,54 +159,43 @@ class GM:
         dE_Q_arr: np.ndarray, float
             Shift in Fermi level due to quantum capacitance
 
-        T: int, float
-            Temperature in K
-
-        E_DOS: np.ndarray
-            1D numpy array that contains all energy points according to DOS points
-
-        DOS: np.ndarray
-            1D numpy array that contains all electron densities according to E_DOS values
-
-        efermi: float
-            Fermi energy in eV
-
         Returns:
         -------
         sigmas: np.ndarray, float
             Computed values (or one value) of surface charge densities
         """
+
+        self.check_param('T')
+
         elementary_charge = 1.6e-13  # micro coulomb
 
         if type(dE_Q_arr) is np.ndarray:
-            E_1 = E_DOS - efermi  # energy range for DOS and not shifted Fermi-Dirac function
-            y_fermi = self.fermi_func(E_1, T)
+            y_fermi = self.fermi_func(self.E, self.T)
 
             sigmas = []
 
             for dE_Q in dE_Q_arr:
-                E_2 = E_DOS - efermi - dE_Q  # energy range for shifted Fermi_Dirac function
-                y_fermi_shifted = self.fermi_func(E_2, T)
-                integrand = DOS * (y_fermi - y_fermi_shifted)
-                sigma = (elementary_charge / self.sheet_area) * integrate.simps(integrand, E_1)
+                E_2 = self.E - dE_Q  # energy range for shifted Fermi_Dirac function
+                y_fermi_shifted = self.fermi_func(E_2, self.T)
+                integrand = self.DOS * (y_fermi - y_fermi_shifted)
+                sigma = (elementary_charge / self.sheet_area) * integrate.simps(integrand, self.E)
                 sigmas.append(sigma)
 
             return sigmas
 
         elif type(dE_Q_arr) is float or type(dE_Q_arr) is np.float64:
-            E_1 = E_DOS - efermi  # energy range for DOS and not shifted Fermi-Dirac function
-            y_fermi = self.fermi_func(E_1, T)
+            y_fermi = self.fermi_func(self.E, self.T)
 
-            E_2 = E_DOS - efermi - dE_Q_arr  # energy range for shifted Fermi_Dirac function
-            y_fermi_shifted = self.fermi_func(E_2, T)
-            integrand = DOS * (y_fermi - y_fermi_shifted)
-            sigma = (elementary_charge / self.sheet_area) * integrate.simps(integrand, E_1)
+            E_2 = self.E - dE_Q_arr  # energy range for shifted Fermi_Dirac function
+            y_fermi_shifted = self.fermi_func(E_2, self.T)
+            integrand = self.DOS * (y_fermi - y_fermi_shifted)
+            sigma = (elementary_charge / self.sheet_area) * integrate.simps(integrand, self.E)
 
             return sigma
         else:
             raise TypeError(f'Invalid type of dE_Q_arr: {type(dE_Q_arr)}')
 
-    def compute_distributions(self, V_std, dE_eta, reverse=False, SIGMA_0=0.1, ACCURACY_SIGMA=1e-3, SIGMA_RANGE=11):
+    def compute_distributions(self, V_std, reverse=False, dE_eta=0, SIGMA_0=0.1, ACCURACY_SIGMA=1e-3, SIGMA_RANGE=5):
         """Function computes Fermi-Dirac and Redox species distributions according to Gerisher-Markus formalism
 
         Parameters:
@@ -173,6 +210,7 @@ class GM:
             if reverse is False the process of electron transfer from electrode to the oxidized state of the
             redox particles is considered and vice versa
         """
+
         def error_E_diff(sigma, E_diff, sigma_Q_arr):
             i_1, i_2 = self.nearest_array_indices(sigma_Q_arr, sigma)
             dE_Q = E_start + E_step * i_1
@@ -181,42 +219,35 @@ class GM:
 
             return (dE_total - E_diff) ** 2
 
+        for var in ['T', 'l', 'C_EDL']:
+            self.check_param(var)
+
         E_step = ACCURACY_SIGMA
         E_start = - SIGMA_RANGE
         E_range = np.arange(E_start, -E_start, E_step)
-        sigma_Q_arr = self.compute_sigma_quantum(E_range, self.T, self.E, self.DOS, self.efermi)
-        sigma_EDL_arr = self.compute_sigma_EDL(self.C_EDL, E_range)
+        sigma_Q_arr = self.compute_sigma_quantum(E_range)
 
         sigma_0 = SIGMA_0
 
-        E_F_redox = -4.5 - V_std - self.efermi + self.vacuum_lvl
+        E_F_redox = -4.5 - self.efermi - V_std + self.vacuum_lvl
 
         # compute equilibrium case
         result = minimize(error_E_diff, np.array([sigma_0]), args=(E_F_redox, sigma_Q_arr))
         sigma_eq = result.x[0]
 
+        self.sigma_eq = sigma_eq
+
         i_1, i_2 = self.nearest_array_indices(sigma_Q_arr, sigma_eq)
         dE_Q_eq = E_start + E_step * i_1
 
-        i_1, i_2 = self.nearest_array_indices(sigma_EDL_arr, sigma_eq)
-        dE_EDL_eq = E_start + E_step * i_1
+        self.dE_Q_eq = dE_Q_eq
 
         # compute the case with nonzero overpotential
         if dE_eta != 0:
-
-            dE_Q_overpot = dE_Q_eq + dE_eta
-            sigma_overpot = self.compute_sigma_quantum(dE_Q_overpot, self.T, self.E,
-                                                       self.DOS, self.efermi)
-            i_1, i_2 = self.nearest_array_indices(sigma_EDL_arr, sigma_overpot)
-            dE_EDl_overpot = E_start + E_step * i_1
-            E_redox_couple = E_F_redox - dE_EDl_overpot
-            E_DOS = self.E - self.efermi
-            E_fermi = self.E - self.efermi - dE_Q_overpot
-            E_DOS_redox = self.E - self.efermi - E_redox_couple
+            pass
         else:
-            E_DOS = self.E - self.efermi
-            E_fermi = self.E - self.efermi - dE_Q_eq
-            E_DOS_redox = self.E - self.efermi - dE_Q_eq
+            E_fermi = self.E - dE_Q_eq
+            E_DOS_redox = self.E - dE_Q_eq
 
         if reverse:
             y_fermi = 1 - self.fermi_func(E_fermi, self.T)
@@ -225,11 +256,10 @@ class GM:
             y_fermi = self.fermi_func(E_fermi, self.T)
             y_redox = self.W_ox(E_DOS_redox, self.T, self.l)
 
-        self.E_DOS = E_DOS
-        self.y_fermi = y_redox
+        self.y_fermi = y_fermi
         self.y_redox = y_redox
 
-        return E_DOS, y_fermi, y_redox
+        return y_fermi, y_redox
 
     '''def compute_k_HET(self, mode, V_std_pot_arr, dE_eta_arr, n_jobs=1):
         """Compute integral k_HET using Geresher-Markus formalism
@@ -366,7 +396,22 @@ class GM:
 
         return df'''
 
+
 if __name__ == '__main__':
+    def nearest_array_indices(array, value):
+        i = 0
+        while value > array[i]:
+            i += 1
+        return i - 1, i
     a = GM()
-    #a.compute_sigma_quantum(np.arange(-1, 1, 0.1), 298, a.E_DOS, a.DOS, a.efermi)
-    a.efermi
+    a.set_params(10, 298, 0.9, 5.265949070860207e-14)
+    y_fermi, y_redox = a.compute_distributions(-1)
+    n_1, n_2 = nearest_array_indices(a.E, a.dE_Q_eq)
+    #print(n_1, n_2, a.dE_Q_eq)
+    #print(integrate.simps(a.DOS[:400], a.E[:400]))
+    plt.fill_between(a.E[:n_2], a.DOS[:n_2])
+    plt.plot(a.E, a.DOS)
+    plt.plot(a.E, y_redox * 10)
+    plt.plot(a.E, y_fermi * 30)
+    plt.xlim([-8, 5])
+    plt.show()
