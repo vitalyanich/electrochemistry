b3b3c674604dd5342f3c14aac599b6d17dd46288
Vitalyanich <vitalyanich@gmail.com>
2021-12-03 11:42:57 +0300

    class AtomicSpinMoments (DDDEC) was added

--- io_data/ddec.py
+++ io_data/ddec.py
@@ -1,12 +1,13 @@
 from matplotlib import colors
 import numpy as np
+from monty.re import regrep
 from ..core.structure import Structure
 
 
-class DDEC:
-    """Class that operates with DDEC output files"""
+class AtomicNetCharges:
+    """Class that operates with DDEC output file DDEC6_even_tempered_net_atomic_charges.xyz"""
     def __init__(self, structure: Structure, net_charges, dipoles_xyz=None,
-                 dipoles_mag=None, Qs=None, quadrupole_tensor_eigs=None):
+                 dipoles_mag=None, Qs=None, quadrupole_tensor_eigs=None, date=None):
         """
         Create a DDEC class object.
         Args:
@@ -17,12 +18,13 @@ class DDEC:
             Qs:
             quadrupole_tensor_eigs:
         """
-        self._stucture = structure
+        self.structure = structure
         self.net_charges = net_charges
         self.dipoles_xyz = dipoles_xyz
         self.dipoles_mag = dipoles_mag
         self.Qs = Qs
         self.quadrupole_tensor_eigs = quadrupole_tensor_eigs
+        self.date = date
 
     @staticmethod
     def from_file(filepath):
@@ -42,6 +44,11 @@ class DDEC:
         file = open(filepath, 'r')
         data = file.readlines()
         file.close()
+
+        patterns = {'date': r'\s+(\d\d\d\d/\d\d/\d\d\s+\d\d:\d\d:\d\d)'}
+        matches = regrep(filepath, patterns)
+        date = matches['date'][0][0][0]
+
         natoms = int(data[0])
         x_axis = data[1].split()[10:13]
         y_axis = data[1].split()[15:18]
@@ -71,17 +78,51 @@ class DDEC:
             quadrupole_tensor_eigs[i] = line_splitted[15:18]
 
         structure = Structure(lattice, species, coords, coords_are_cartesian=True)
-        return DDEC(structure, net_charges, dipoles_xyz, dipoles_mag, Qs, quadrupole_tensor_eigs)
+        return AtomicNetCharges(structure, net_charges, dipoles_xyz, dipoles_mag, Qs, quadrupole_tensor_eigs, date)
 
-    @property
-    def structure(self):
-        return self._stucture
 
-    class MidpointNormalize(colors.Normalize):
-        def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
-            self.midpoint = midpoint
-            colors.Normalize.__init__(self, vmin, vmax, clip)
+class AtomicSpinMoments:
+    """Class that operates with DDEC output file DDEC6_even_tempered_atomic_spin_moments.xyz"""
+    def __init__(self, structure: Structure, spin_moments, date):
+        self.structure = structure
+        self.spin_moments = spin_moments
+        self.date = date
 
-        def __call__(self, value, clip=None):
-            x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
-            return np.ma.masked_array(np.interp(value, x, y))
+    @staticmethod
+    def from_file(filepath):
+        file = open(filepath, 'r')
+        data = file.readlines()
+        file.close()
+
+        patterns = {'date': '\s+(\d\d\d\d/\d\d/\d\d\s+\d\d:\d\d:\d\d)'}
+        matches = regrep(filepath, patterns)
+        date = matches['date'][0][0][0]
+
+        natoms = int(data[0])
+        x_axis = data[1].split()[10:13]
+        y_axis = data[1].split()[15:18]
+        z_axis = data[1].split()[20:23]
+        lattice = np.array([x_axis, y_axis, z_axis], dtype=np.float32)
+
+        coords = np.zeros((natoms, 3))
+        species = []
+        spin_moments = np.zeros(natoms)
+
+        for i, j in enumerate(range(2, 2 + natoms)):
+            line_splitted = data[j].split()
+            species += [line_splitted[0]]
+            coords[i] = line_splitted[1:4]
+            spin_moments[i] = line_splitted[4]
+
+        structure = Structure(lattice, species, coords, coords_are_cartesian=True)
+        return AtomicSpinMoments(structure, spin_moments, date)
+
+
+class MidpointNormalize(colors.Normalize):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
