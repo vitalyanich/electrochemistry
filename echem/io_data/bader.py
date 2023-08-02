f1e6bf35c006098443de9854991b59d734924711
Vitaliy <40563379+vitalyanich@users.noreply.github.com>
2023-08-02 21:20:36 +0300

    Fixed bags with Bader class

--- echem/io_data/bader.py
+++ echem/io_data/bader.py
@@ -6,10 +6,12 @@ from nptyping import NDArray, Shape, Number
 class ACF:
     def __init__(self,
                  coords: NDArray[Shape['Natoms, 3'], Number],
-                 nelec_per_atom: NDArray[Shape['Natoms'], Number]):
+                 nelec_per_atom: NDArray[Shape['Natoms'], Number],
+                 spin_per_atom: NDArray[Shape['Natoms'], Number] = None):
         self.coords = coords
         self.nelec_per_atom = nelec_per_atom
         self.nelec_per_isolated_atom = None
+        self.spin_per_atom = spin_per_atom
 
     @staticmethod
     def from_file(filepath: str | Path):
@@ -19,13 +21,19 @@ class ACF:
         file = open(filepath)
         file.readline()
         line = file.readline()
-
+        ncols = len(line.split('+'))
         if '+' in line:
-            data = np.genfromtxt(filepath, skip_header=2, skip_footer=5, delimiter='|')
+            if ncols == 7:
+                data = np.genfromtxt(filepath, skip_header=2, skip_footer=5, delimiter='|')
+                return ACF(data[:, 1:4], data[:, 4])
+            elif ncols == 8:
+                data = np.genfromtxt(filepath, skip_header=2, skip_footer=7, delimiter='|')
+                return ACF(data[:, 1:4], data[:, 4], data[:, 5])
+            else:
+                raise IOError(f'Can parse ACF.dat with 7 or 8 columns, but {ncols=} was given')
         else:
             data = np.genfromtxt(filepath, skip_header=2, skip_footer=4)
-
-        return ACF(data[:, 1:4], data[:, 4])
+            return ACF(data[:, 1:4], data[:, 4])
 
     def get_charge(self,
                    nelec_per_isolated_atom: NDArray[Shape['Natoms'], Number] | None = None):
